b6091e80e4551cc17d71eff578d943388f31d72a
Aleksandra1311 <77723241+Aleksandra1311@users.noreply.github.com>
2021-08-28 10:06:04 +0300

    Added test task
    Added test task

--- JS_1/script.js
+++ JS_1/script.js
@@ -3,3 +3,16 @@ $(() => {
         $('body').toggleClass('stop-scrolling');
     });
 })
+
+// 2й вариант ниже:
+
+// $(() => {
+//     const STOP_SCROLLING = 'stop-scrolling';
+    
+//     const $span = $('span');
+//     const $body = $('body');
+
+//     $($span).hover(function() {
+//         $($body).toggleClass(STOP_SCROLLING);
+//     });
+// })
